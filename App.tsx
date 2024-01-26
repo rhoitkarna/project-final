035a1af51c4970c458d7035ce591341b9f11ad7a
kirankckc <kiranchaudharykc5@gmail.com>
2024-01-26 22:53:19 +0545

    mobile app

--- App.tsx
+++ App.tsx
@@ -1,117 +1,322 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
+  Image,
   StatusBar,
   StyleSheet,
   Text,
+  Platform,
+  Dimensions,
   useColorScheme,
   View,
+  TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
+import axios from 'axios';
+import Config from 'react-native-config';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import PermissionsService, { isIOS } from './Permissions';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Importing images
+import background from './images/background.jpg';
+import clean from './images/clean.png';
+import camera from './images/camera.png';
+import gallery from './images/gallery.png';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+axios.interceptors.request.use(
+  async (config) => {
+    let request = config;
+    request.headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+    request.url = configureUrl(config.URL);
+    return request;
+  },
+  (error) => error
+);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+
+export const { height, width } = Dimensions.get('window');
+
+export const configureUrl = (url) => {
+  let authUrl = url;
+  if (url && url[url.length - 1] === '/') {
+    authUrl = url.substring(0, url.length - 1);
+  }
+  return authUrl;
+};
+
+export const fonts = {
+  Bold: { fontFamily: 'Roboto-Bold' },
+};
+
+const options = {
+  mediaType: 'photo',
+  quality: 1,
+  width: 256,
+  height: 256,
+  includeBase64: true,
+};
+
+const App = () => {
+  const [result, setResult] = useState('');
+  const [label, setLabel] = useState('');
   const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
+  const [image, setImage] = useState('');
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+//   const getPredication = async (params) => {
+//     return new Promise((resolve, reject) => {
+//       var bodyFormData = new FormData();
+//       bodyFormData.append('file', params);
+//       const url = Config.URL;
+//       return axios
+//         .post(url, bodyFormData)
+//         .then((response) => {
+//           resolve(response);
+//         })
+//         .catch((error) => {
+//           setLabel('Failed to predict.');
+//           reject('err', error);
+//         });
+//     });
+//   };
+
+const getPredication = async (params) => {
+console.log("Getting predictions")
+  return new Promise((resolve, reject) => {
+    var bodyFormData = new FormData();
+    bodyFormData.append('file', params);
+    const url = "http://localhost:8000/predict";
+    console.log("Url", url)
+    // Specify headers for this request
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+
+//     return axios
+//       .post("http://192.168.1.75:8000/predict", bodyFormData, config)
+//       .then((response) => {
+//         console.log("Res", response)
+//         resolve(response);
+//       })
+//       .catch((error) => {
+//       console.log("Error Res", error)
+//         setLabel('Failed to predict.');
+//         reject('err', error);
+//       });
+
+      return fetch("http://192.168.1.75:8000/predict", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          body: bodyFormData
+      }).then((response) => {
+                console.log("Res", response)
+                resolve(response.json());
+              })
+              .catch((error) => {
+              console.log("Error Res", error)
+                setLabel('Failed to predict.');
+                reject('err', error);
+              });
+  });
+};
+
+
+
+  const manageCamera = async (type) => {
+    try {
+      if (!(await PermissionsService.hasCameraPermission())) {
+        return [];
+      } else {
+        if (type === 'Camera') {
+          openCamera();
+        } else {
+          openLibrary();
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const openCamera = async () => {
+    launchCamera(options, async (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const uri = response?.assets[0]?.uri;
+        const path = Platform.OS !== 'ios' ? uri : 'file://' + uri;
+        getResult(path, response);
+      }
+    });
+  };
+
+  const clearOutput = () => {
+    setResult('');
+    setImage('');
+  };
+
+  const getResult = async (path, response) => {
+    setImage(path);
+    setLabel('Predicting...');
+    setResult('');
+    const params = {
+      uri: path,
+      name: response.assets[0].fileName,
+      type: response.assets[0].type,
+    };
+    const res = await getPredication(params);
+    console.log("Response from getresult", res)
+
+    function toTitleCase(str) {
+      return str.replace(
+        /\w\S*/g,
+        function(txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+      );
+    }
+    if (res?.class) {
+    prediction = res.class;
+    disease = prediction.split("_").join(" ")
+      setLabel(toTitleCase(disease));
+      setResult(res.confidence * 100);
+    } else {
+      setLabel('Failed to predict');
+    }
+  };
+
+  const openLibrary = async () => {
+    launchImageLibrary(options, async (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const uri = response.assets[0].uri;
+        const path = Platform.OS !== 'ios' ? uri : 'file://' + uri;
+        getResult(path, response);
+      }
+    });
+  };
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={[backgroundStyle, styles.outer]}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ImageBackground blurRadius={10} source={background} style={{ height: height, width: width }} />
+      <Text style={styles.title}>{'FarmEasy: Paddy\nDisease Detection'}</Text>
+      <TouchableOpacity onPress={clearOutput} style={styles.clearStyle}>
+        <Image source={clean} style={styles.clearImage} />
+      </TouchableOpacity>
+      {(image?.length && <Image source={{ uri: image }} style={styles.imageStyle} />) || null}
+      {(result && label && (
+        <View style={styles.mainOuter}>
+          <Text style={[styles.space, styles.labelText]}>
+            {'Label: \n'}
+            <Text style={styles.resultText}>{label}</Text>
+          </Text>
+          <Text style={[styles.space, styles.labelText]}>
+            {'Confidence: \n'}
+            <Text style={styles.resultText}>{parseFloat(result).toFixed(2) + '%'}</Text>
+          </Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      )) ||
+        (image && <Text style={styles.emptyText}>{label}</Text>) || (
+          <Text style={styles.emptyText}>
+            Use below buttons to select a picture of a paddy plant leaf.
+          </Text>
+        )}
+      <View style={styles.btn}>
+        <TouchableOpacity activeOpacity={0.9} onPress={() => manageCamera('Camera')} style={styles.btnStyle}>
+          <Image source={camera} style={styles.imageIcon} />
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.9} onPress={() => manageCamera('Photo')} style={styles.btnStyle}>
+          <Image source={gallery} style={styles.imageIcon} />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  title: {
+    alignSelf: 'center',
+    position: 'absolute',
+    top: (isIOS && 35) || 10,
+    fontSize: 30,
+    ...fonts.Bold,
+    color: '#FFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  clearImage: { height: 40, width: 40, tintColor: '#FFF' },
+  mainOuter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    top: height / 1.6,
+    alignSelf: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  outer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  btn: {
+    position: 'absolute',
+    bottom: 40,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  btnStyle: {
+    backgroundColor: '#FFF',
+    opacity: 0.8,
+    marginHorizontal: 30,
+    padding: 20,
+    borderRadius: 20,
+  },
+  imageStyle: {
+    marginBottom: 50,
+    width: width / 1.5,
+    height: width / 1.5,
+    borderRadius: 20,
+    position: 'absolute',
+    borderWidth: 0.3,
+    borderColor: '#FFF',
+    top: height / 4.5,
+  },
+  clearStyle: {
+    position: 'absolute',
+    top: 100,
+    right: 30,
+    tintColor: '#FFF',
+    zIndex: 10,
+  },
+  space: { marginVertical: 10, marginHorizontal: 10 },
+  labelText: { color: '#FFF', fontSize: 20, ...fonts.Bold },
+  resultText: { fontSize: 32, ...fonts.Bold },
+  imageIcon: { height: 40, width: 40, tintColor: '#000' },
+  emptyText: {
+    position: 'absolute',
+    top: height / 1.6,
+    alignSelf: 'center',
+    color: '#FFF',
+    fontSize: 20,
+    maxWidth: '70%',
+    ...fonts.Bold,
   },
 });
 
---
+++ Permissions.js
@@ -0,0 +1,173 @@
+//import {Alert, Platform} from 'react-native';
+//import {
+//  check,
+//  PERMISSIONS,
+//  RESULTS,
+//  request,
+//  openSettings,
+//} from 'react-native-permissions';
+//
+//export const isIOS = Platform.OS === 'ios';
+//
+//function showAlert(msg) {
+//  Alert.alert('', msg, [
+//    {
+//      text: 'Cancel',
+//      onPress: () => console.log('Cancel Pressed'),
+//      style: 'cancel',
+//    },
+//    {
+//      text: 'Settings',
+//      onPress: () => {
+//        openSettings().catch(() => console.warn('cannot open settings'));
+//      },
+//    },
+//  ]);
+//}
+//
+//const hasCameraPermission = async (withAlert = true) => {
+//  try {
+//    const permission = isIOS
+//      ? PERMISSIONS.IOS.CAMERA
+//      : PERMISSIONS.ANDROID.CAMERA;
+//    const response = await check(permission);
+//    let camera;
+//    if (response.camera !== RESULTS.GRANTED) {
+//      camera = await request(permission);
+//    }
+//    if (camera === RESULTS.DENIED || camera === RESULTS.BLOCKED) {
+//      if (withAlert) {
+//        showAlert(
+//          'Permission not granted for camera. You will not able to use camera in this application.',
+//        );
+//      }
+//      return false;
+//    }
+//    return true;
+//  } catch (error) {
+//    console.log(error);
+//    return false;
+//  }
+//};
+//
+//const hasPhotoPermission = async (withAlert = true) => {
+//  try {
+//    const permission = isIOS
+//      ? PERMISSIONS.IOS.PHOTO_LIBRARY
+//      : PERMISSIONS.ANDROID.WRITE_EXTERNAL_StorageService;
+//    const response = await check(permission);
+//    let photo;
+//    if (response.photo !== RESULTS.GRANTED) {
+//      photo = await request(permission);
+//    }
+//    if (photo === RESULTS.DENIED || photo === RESULTS.BLOCKED) {
+//      if (withAlert) {
+//        showAlert(
+//          'Permission not granted for photos. You will not able to get photos in this application.',
+//        );
+//      }
+//      return false;
+//    }
+//    return true;
+//  } catch (error) {
+//    console.log(error);
+//    return false;
+//  }
+//};
+//
+//const PermissionsService = {
+//  hasCameraPermission,
+//  hasPhotoPermission,
+//};
+//
+//export default PermissionsService;
+
+import { Alert, Platform } from 'react-native';
+import {
+  check,
+  PERMISSIONS,
+  RESULTS,
+  request,
+  openSettings,
+} from 'react-native-permissions';
+
+export const isIOS = Platform.OS === 'ios';
+
+function showAlert(msg) {
+  Alert.alert('', msg, [
+    {
+      text: 'Cancel',
+      onPress: () => console.log('Cancel Pressed'),
+      style: 'cancel',
+    },
+    {
+      text: 'Settings',
+      onPress: () => {
+        openSettings().catch(() => console.warn('Cannot open settings'));
+      },
+    },
+  ]);
+}
+
+const hasCameraPermission = async (withAlert = true) => {
+  try {
+    const permission = isIOS
+      ? PERMISSIONS.IOS.CAMERA
+      : PERMISSIONS.ANDROID.CAMERA;
+    const response = await check(permission);
+
+    if (response === RESULTS.GRANTED) {
+      return true;
+    } else {
+      const result = await request(permission);
+      if (result === RESULTS.GRANTED) {
+        return true;
+      } else {
+        if (withAlert) {
+          showAlert(
+            'Permission not granted for camera. You will not be able to use the camera in this application.',
+          );
+        }
+        return false;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+const hasPhotoPermission = async (withAlert = true) => {
+  try {
+    const permission = isIOS
+      ? PERMISSIONS.IOS.PHOTO_LIBRARY
+      : PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE;
+    const response = await check(permission);
+
+    if (response === RESULTS.GRANTED) {
+      return true;
+    } else {
+      const result = await request(permission);
+      if (result === RESULTS.GRANTED) {
+        return true;
+      } else {
+        if (withAlert) {
+          showAlert(
+            'Permission not granted for photos. You will not be able to get photos in this application.',
+          );
+        }
+        return false;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+const PermissionsService = {
+  hasCameraPermission,
+  hasPhotoPermission,
+};
+
+export default PermissionsService;
